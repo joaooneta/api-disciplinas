2b3cbf0dff0717b25fb76177b5aee919dbb4b5df
João Matheus Oneta <joaomatheusoneta01@gmail.com>
2025-04-14 21:10:35 -0300

    Primeiro commit

---
+++ disciplinasRepository.js
@@ -0,0 +1,80 @@
+const { v4: uuidv4 } = require("uuid");
+const porta = 3000;
+
+const disciplinas = [
+  {
+    id: uuidv4(),
+    nome: "Desenvolvimento Front-End",
+    cargaHoraria: 10,
+    obrigatoria: true,
+  },
+  {
+    id: uuidv4(),
+    nome: "Desenvolvimento Back-End",
+    cargaHoraria: 20,
+    obrigatoria: false,
+  },
+  {
+    id: uuidv4(),
+    nome: "Design de Interfaces",
+    cargaHoraria: 30,
+    obrigatoria: true,
+  },
+  {
+    id: uuidv4(),
+    nome: "APIs RESTful",
+    cargaHoraria: 40,
+    obrigatoria: false,
+  },
+];
+
+function listar(nome) {
+  if (nome) {
+    const disciplinasFiltradas = disciplinas.filter((d) =>
+      d.nome.toLowerCase().includes(nome.toLowerCase())
+    );
+
+    return disciplinasFiltradas;
+  }
+
+  return disciplinas;
+}
+
+function consultar(id) {
+  return disciplinas.find((d) => d.id === id);
+}
+
+function adicionar(disciplina) {
+  const novaDisciplina = { id: uuidv4(), ...disciplina };
+  disciplinas.push(novaDisciplina);
+
+  return novaDisciplina;
+}
+
+function atualizar(id, disciplinaAtualizada) {
+  const i = disciplinas.findIndex((d) => d.id === id);
+  if (i === -1) return null;
+
+  disciplinas[i] = { ...disciplinas[i], ...disciplinaAtualizada };
+
+  return disciplinas[i];
+}
+
+function remover(id) {
+  const i = disciplinas.findIndex((d) => d.id === id);
+  if (i === -1) return null;
+
+  const disciplinaRemovida = disciplinas[i];
+
+  disciplinas.splice(i, 1);
+
+  return disciplinaRemovida;
+}
+
+module.exports = {
+  listar,
+  consultar,
+  adicionar,
+  atualizar,
+  remover,
+};
---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const repo = require("./disciplinasRepository");
+
+app.use(express.json());
+
+app.get("/disciplinas", (req, res) => {
+  const { nome } = req.query;
+  const resultado = repo.listar(nome);
+
+  res.json(resultado);
+});
+
+app.get("/disciplinas/:id", (req, res) => {
+  const disciplina = repo.consultar(req.params.id);
+
+  if (!disciplina)
+    return res.status(404).json({ mensagem: "Disciplina não encontrada!" });
+
+  res.json(disciplina);
+});
+
+app.post("/disciplinas", (req, res) => {
+  const novaDisciplina = repo.adicionar(req.body);
+
+  res.status(201).json(novaDisciplina);
+});
+
+app.put("/disciplinas/:id", (req, res) => {
+  const disciplinaAtualizada = repo.atualizar(req.params.id, req.body);
+
+  if (!disciplinaAtualizada)
+    return res.status(404).json({ mensagem: "Disciplina não encontrada!" });
+
+  res.json(disciplinaAtualizada);
+});
+
+app.delete("/disciplinas/:id", (req, res) => {
+  const disciplinaRemovida = repo.remover(req.params.id);
+
+  if (!disciplinaRemovida)
+    return res.status(404).json({ mensagem: "Disciplina não encontrada!" });
+
+  res.json(disciplinaRemovida);
+});
+
+app.listen(port, () => {
+  console.log("Rodando...");
+});
